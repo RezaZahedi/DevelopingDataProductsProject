1bbcc93cceded2631c13498a6d5e185c2887ee88
Reza Zahedi <reza.b.e@hotmail.com>
2018-11-06 00:58:55 +0330

    server and ui are almost complete, needs commenting

--- Circle/server.R
+++ Circle/server.R
@@ -12,14 +12,32 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
+  output$circlePlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+       cx = 10
+       cy = 10
+       rad <- 5
+       n <- as.integer(input$n)
+       lineWidth <- 7
+       if(n %% 2 == 0 ){
+            theta <- seq(from = pi/n , to = 2 * pi + pi/n,
+                         length.out = n+1)
+       }
+       else{
+            theta <- seq(from = pi/2 , to = 2 * pi + pi/2,
+                         length.out = n+1)
+       }
+       x <- cx + rad * cos(theta)
+       y <- cy + rad * sin(theta)
+       
+       red <- 193
+       green <- 70
+       blue <- 13
+       plot(x, y, ylim = input$ylim, xlim = input$xlim, type = "l", bty = "n",
+            col = rgb(input$red, input$green, input$blue, max=255),
+            lwd = lineWidth)
+       points(10, 10, pch = 19)
+       
     
   })
   
--- Circle/ui.R
+++ Circle/ui.R
@@ -13,21 +13,44 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Coloring n-edged shape!"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+         selectInput("n", "Number of Edges:", 2:20, 5),
+         
+         sliderInput("red", "Red Intensity",
+              min = 1, max = 255, value = 193
+         ),
+         
+         sliderInput("green", "Green Intensity",
+              min = 1, max = 255, value = 70
+         ),
+         
+         sliderInput("blue", "Blue Intensity",
+              min = 1, max = 255, value = 13
+         ),
+         
+         sliderInput("xlim", "X Axis limits:",
+                     min = -25, max = 25, value = c(0, 20)),
+         
+         sliderInput("ylim", "Y Axis limits:",
+                     min = -25, max = 25, value = c(5, 15))
     ),
+    
+    
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("circlePlot"),
+       h3("Discription:"),
+       p("This app is designed to draw an n-edged shape, and color it usig the
+         the Red, Green and Blue components of a user specified color."),
+       h3("Inputs:"),
+       p("Number of edges"), br(),
+       p("Red, green and blue intesity"), br(),
+       p("X and Y axis limits, to change something :D")
     )
   )
 ))
